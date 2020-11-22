dfa7125a651311332972dc29cd74f7662894a175
kamiya-s-max <s.kamiya.f15@gmail.com>
2020-11-22 10:07:49 +0900

    削除機能の実装

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::EmployeesController < ApiController
-  before_action :set_employee, only: [:show, :update]
+  before_action :set_employee, only: [:show, :update, :destroy]
 
   rescue_from Exception, with: :render_status_500
 
@@ -29,6 +29,11 @@ class Api::V1::EmployeesController < ApiController
     else
       render json: { errors: @employee.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @employee.destroy!
+    head :no_content
   end
 
   private
